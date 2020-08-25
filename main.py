450849bab31ccf8398103a341fe23626f42a37b3
Aaryan Goel <apg5729@psu.edu>
2020-08-25 18:48:15 +0000

    how age was formated

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-name = input("enter name: ")
-print ("hello world")
+name = input("Enter your name: ")
+print ("hello, {name}!")
+age = input("Enter your age: ")
+age = int(age)
+print (f"You will be {age+1} next year.")
